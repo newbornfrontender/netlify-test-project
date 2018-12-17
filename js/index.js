2c028c5f3e17db7c18cfb8bba211b6676881f1a6
Infant Frontender <newbornfrontender@gmail.com>
2018-12-17 16:15:36 +0300

    Add scripts

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const fetchUsers = async () =>
+  await (await fetch('http://localhost:9000/get-users')).json();
+
+fetchUsers().then((data) => {
+  const userList = document.querySelector('#users');
+
+  data.forEach(({ login, html_url }) => {
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+
+    const link = document.createElement('a');
+    link.appendChild(document.createTextNode(login));
+    link.href = html_url;
+    link.target = '_blank';
+
+    li.appendChild(link);
+
+    userList.appendChild(li);
+  });
+});
